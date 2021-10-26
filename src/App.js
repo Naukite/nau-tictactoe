31003bebee687a7caf823029ffae0b79db87fd95
Naukite <naukite@gmail.com>
2021-10-26 13:46:01 +0100

    feat: se añade la posibilidad de volver hacia atras usando history como un vector que almacena los estados del juego

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ const BoardRow = (props) => {
     boardEntries.push(<BoardEntry key={entryKey} entryKey={entryKey} i={props.i} j={j} 
                                   value={props.row[j]} onEntryClick={props.onEntryClick} disabled={props.disabled}/>)
   }
-  return boardEntries
+  return (
+    <div>
+      {boardEntries}
+    </div>
+  )
 }
 
 const DisplayPlayer = (props) => {
@@ -43,70 +47,97 @@ const DisplayStepList = (props) => {
 }
 
 const useBoardState = (size) => {  
-  const [state, setState] = useState({
+  const stateObject = {
     step: 1,
     player: 0,
     board: new BoardObject(size),
     wonGame: false
+  }
+
+  const [state, setState] = useState({
+    history: [stateObject]
   })
 
   const setBoardState = (prevState, i, j, players) => {
     setState((prevState) => {
-      const newBoard = prevState.board.clone()      
-      const wonGame = newBoard.setValue(i, j, players[prevState.player])
+      const lastState = prevState.history[prevState.history.length - 1]
+      const lastStep = lastState.step
+      const lastPlayer = lastState.player
+      const newBoard = lastState.board.clone()            
+      const wonGame = newBoard.setValue(i, j, players[lastState.player])
+      const newHistory = [...prevState.history]      
       if (wonGame) {
-        return {
-          step: prevState.step,
-          player: prevState.player,
+        newHistory.push({
+          step: lastStep,
+          player: lastPlayer,
           board: newBoard,
           wonGame: wonGame
+        })
+        return {
+          history: newHistory
         }
       }
-      return {
-        step: prevState.step + 1,
-        player: (prevState.player + 1) % 2,
+      newHistory.push({
+        step: lastStep + 1,
+        player: (lastPlayer + 1) % 2,
         board: newBoard,
         wonGame: wonGame
+      })
+      return {
+        history: newHistory
       }
     })
   }
 
-  return [state, setBoardState]
+  const jumpToState = (stateIndex) => {
+    setState(() => {
+      const newHistory = []
+      for (let i = 0; i <= stateIndex; i++) {
+        newHistory.push(state.history[i])
+      }
+      return {
+        history: newHistory
+      }      
+    })
+  }
+
+  return [state, setBoardState, jumpToState]
 }
 
 
 const Board = (props) => {
   const players = ['X', 'O']
-  const [boardState, setBoardState] = useBoardState(props.size)
+  const [boardState, setBoardState, jumpToState] = useBoardState(props.size)
 
   const handleEntryClick = (event) => {
     const coords = event.target.id.split("_")
     const i = parseInt(coords[0])
-    const j = parseInt(coords[1])
-    if (boardState.board.getValue(i, j)) {
+    const j = parseInt(coords[1])    
+    const lastState = boardState.history[boardState.history.length - 1]
+    if (lastState.board.getValue(i, j)) {
       return
     }
 
-    setBoardState(boardState, i, j, players)
+    setBoardState(lastState, i, j, players)
   }
 
-  const handleStepClick = (event) => {
-    console.log(event.target.id)
+  const handleStepClick = (event) => {  
+    jumpToState(parseInt(event.target.id))    
   }
 
   const rowIndexes = [...Array(props.size).keys()]
-
+  const currentState = boardState.history[boardState.history.length - 1]
   return(
     <React.Fragment>
-      <DisplayPlayer player={players[boardState.player]} />
+      <DisplayPlayer player={players[currentState.player]} />
       <br/>      
       {
-        rowIndexes.map(i => { return <BoardRow key={i} i={i} row={boardState.board.getRow(i)} onEntryClick={handleEntryClick} 
-                                                disabled={boardState.wonGame}/>})
+        rowIndexes.map(i => { return <BoardRow key={i} i={i} row={currentState.board.getRow(i)} onEntryClick={handleEntryClick} 
+                                                disabled={currentState.wonGame}/>})
       }
-      <DisplayWon wonGame={boardState.wonGame} player={players[boardState.player]} restartClick={props.restartClick}/> 
+      <DisplayWon wonGame={currentState.wonGame} player={players[currentState.player]} restartClick={props.restartClick}/> 
       <br/><br/>
-      <DisplayStepList step={boardState.step} stepClick={handleStepClick} />
+      <DisplayStepList step={currentState.step} stepClick={handleStepClick} />
     </React.Fragment>
   )
 }
